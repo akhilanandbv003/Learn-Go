6dea2fad65da9fc5153231a06f1c765ef2c5e2b7
akhilanand benkal venkanna <akhilanand.venkanna@monsanto.com>
2018-10-15 23:54:49 -0500

    wip

---
+++ songs-api/main.go
@@ -0,0 +1,25 @@
+package songs_api
+
+import (
+	"encoding/json"
+	"fmt"
+	//"log"
+	//"net/http"
+	"github.com/gorilla/mux"
+)
+type Song struct {
+	ID int `json:id`
+	Title string `json:title`
+	Artist string `json:author`
+	Year string `json:year`
+}
+
+var songs [] Song
+
+func main()  {
+router :=mux.NewRouter()
+
+router.HandleFunc
+
+
+}
